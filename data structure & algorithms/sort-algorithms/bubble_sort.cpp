9a93c1f59bda412f9d5ad4f93ca3d3f54cec6f02
duytoxic <thongthoathich24h@gmail.com>
2020-12-11 21:03:49 +0700

    update

--- data structure & algorithms/sort-algorithms/bubble_sort.cpp
+++ data structure & algorithms/sort-algorithms/bubble_sort.cpp
@@ -11,23 +11,46 @@ using namespace std;
     - Chậm nhất (vì phải duyệt tất cả các phần tử)
 */
 
-void Xuatmang(int arr[], int n) {
-    for (int i = 0; i<n; i++) {
-        cout<<arr[i]<<'\t';
+void Xuatmang(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << '\t';
     }
 }
 
-void swap(int *a, int *b) {
+void swap(int *a, int *b)
+{
     int temp = *a;
-    *a=*b;
-    *b=temp;
+    *a = *b;
+    *b = temp;
 }
 
-void bubbleSort(int arr[], int &n) {
-    for (int i = 0; i< n-1; i++) {
-        for (int j = n-1; j > i; j--) {
-            if(arr[i] > arr[j]) {
-                swap(&arr[i], &arr[j]);
+void bubbleSort(int arr[], int &n)
+{ //nổi bật phần tử nhỏ nhất lên trước
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = n - 1; j > i; j--)
+        {
+            if (arr[j] > arr[j - 1])
+            {
+                swap(&arr[j], &arr[j - 1]);
+            }
+        }
+        Xuatmang(arr, n);
+        cout << endl;
+    }
+}
+
+void bubbleSortDESC(int arr[], int &n)
+{ //nổi bật phần tử lớn nhất lên trước
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - 1 - i; j++)
+        {
+            if (arr[j] < arr[j + 1])
+            {
+                swap(&arr[j], &arr[j + 1]);
             }
         }
         Xuatmang(arr, n);
@@ -37,11 +60,13 @@ void bubbleSort(int arr[], int &n) {
 
 int main()
 {
-    int a[] = {12,2,8,5,1,6,4,15};
+    int a[] = {12, 2, 8, 5, 1, 6, 4, 15};
     int lenght = sizeof(a) / sizeof(int);
-    Xuatmang(a,lenght);
-    cout<<"\nSau khi sắp xếp"<<endl;
+    Xuatmang(a, lenght);
+    cout << "\nSorted : " << endl;
     bubbleSort(a, lenght);
-    Xuatmang(a,lenght);
-    cout<<endl;
+    Xuatmang(a, lenght);
+    cout << "\nSorted DESC: " << endl;
+    bubbleSortDESC(a, lenght);
+    cout << endl;
 }
